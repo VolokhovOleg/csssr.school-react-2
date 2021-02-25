16f96d4ded0d2648521cb72db8cba82d4591ac3f
Oleg Volokhov <volokhov@creativepeople.agency>
2021-02-25 03:41:17 +0300

    Сделано задание 4-1

--- src/reconcile.js
+++ src/reconcile.js
@@ -2,17 +2,28 @@ import instantiate from "./instantiate";
 // eslint-disable-next-line import/no-cycle
 import reconcileChildren from "./reconcile-children";
 import updateDomProperties from "./update-dom-properties";
+import componentPerformance from "./component-perfomance";
+
+componentPerformance.startTracking();
 
 function reconcile(parentDom, instance, element) {
   if (instance === null) {
     // Создаём инстанс
+    componentPerformance.start(`Создаём инстанс`);
     const newInstance = instantiate(element);
     parentDom.appendChild(newInstance.dom);
+    componentPerformance.end(`Создаём инстанс`);
+    componentPerformance.measure(`Создаём инстанс`);
+    componentPerformance.print();
     return newInstance;
   }
   if (element === null) {
     // Убираем инстанс
+    componentPerformance.start(`Убираем инстанс`);
     parentDom.removeChild(instance.dom);
+    componentPerformance.end(`Убираем инстанс`);
+    componentPerformance.measure(`Убираем инстанс`);
+    componentPerformance.print();
     return null;
   }
   if (
@@ -22,13 +33,18 @@ function reconcile(parentDom, instance, element) {
     typeof element.type === `string`
   ) {
     // Обновляем инстанс DOM-элемента
+    componentPerformance.start(`Обновляем инстанс DOM-элемента`);
     updateDomProperties(instance.dom, instance.element.props, element.props);
+    componentPerformance.end(`Обновляем инстанс DOM-элемента`);
+    componentPerformance.measure(`Обновляем инстанс DOM-элемента`);
+    componentPerformance.print();
     /* eslint-disable no-param-reassign */
     instance.childInstances = reconcileChildren(instance, element); // eslint-disable-line no-use-before-define
     instance.element = element;
     return instance;
   }
   // Обновляем инстанс компонента
+  componentPerformance.start(`Обновляем инстанс компонента`);
   instance.publicInstance.props = element.props;
   const childElement = instance.publicInstance.render();
   const oldChildInstance = instance.childInstance;
@@ -36,8 +52,13 @@ function reconcile(parentDom, instance, element) {
   instance.dom = childInstance.dom;
   instance.childInstance = childInstance;
   instance.element = element;
-
+  componentPerformance.end(`Обновляем инстанс компонента`);
+  componentPerformance.measure(`Обновляем инстанс компонента`);
+  componentPerformance.print();
   return instance;
 }
+
+componentPerformance.print();
+componentPerformance.stopTracking();
 
 export default reconcile;
--- src/update-instance.js
+++ src/update-instance.js
@@ -1,10 +1,20 @@
 import reconcile from "./reconcile";
+import componentPerformance from "./component-perfomance";
+
+componentPerformance.startTracking();
 
 function updateInstance(internalInstance) {
   const { dom, element } = internalInstance;
   const parentDom = dom.parentNode;
+  componentPerformance.start(`Update instance`);
 
   reconcile(parentDom, internalInstance, element);
+
+  componentPerformance.end(`Update instance`);
+  componentPerformance.measure(`Update instance`);
 }
+
+componentPerformance.print();
+componentPerformance.stopTracking();
 
 export default updateInstance;
